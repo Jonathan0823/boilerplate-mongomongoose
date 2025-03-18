e70a89065ea3f9be6d7e83401f85e66f77943ca0
Guinn <eguinjonathan08@gmail.com>
2025-03-19 04:38:28 +0700

    feat: add function to find and update

--- myApp.js
+++ myApp.js
@@ -66,7 +66,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findByIdAndUpdate(personName, { age: ageToSet }, { new: true }, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const removeById = (personId, done) => {
